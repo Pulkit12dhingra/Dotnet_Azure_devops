f452e4ff524402ce93cf9b75ea986a3b03590902
Pulkit Dhingra <pulkit12dhingra@gmail.com>
2022-08-16 13:07:32 +0530

    Add files via upload

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using Assignmnet2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignmnet2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DisplayValue(int t1, int t2)
+        {
+            Values values = new Values();
+            int a = t1;
+            int b = t2;
+
+            values.add = a + b;
+            values.sub = a - b;
+            values.mul = a * b;
+            values.divQue = a / b;
+            values.divRem = a % b;
+
+            return View(values);
+        }
+
+
+        public IActionResult Index()
+        {
+            Values val = new Values();
+            return View();
+        }
+
+        public int Add(int a, int b) {
+            return a+ b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        
+
+
+        public ActionResult Display(Values val)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Models/Values.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignmnet2.Models
+{
+    public class Values
+    { 
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int add { get; set; }
+        public int sub { get; set; }
+        public int mul { get; set; }
+        public int divRem { get; set; }
+
+        public int divQue { get; set; }
+    }
+}
